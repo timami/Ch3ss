7f9fb62175d782648df94b8ca933d7edee04703c
iTwicKz <twickes32@ufl.edu>
2015-04-04 11:22:42 -0400

    Initial file creation

---
+++ Game.cpp
@@ -0,0 +1,13 @@
+class Game{
+	private:
+		bool start;
+		bool winP1;
+		bool win P2;
+		
+
+
+PieceGrouping(){
+	
+}
+
+}
---
+++ Piece.h
@@ -0,0 +1,7 @@
+#include <string>
+using namespace std;
+
+class Piece{
+	private:
+		bool dead;
+		int move[2];
